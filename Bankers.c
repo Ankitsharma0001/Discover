681429744e0906e5b3462a645211346fbc296e7f
Ankitsharma0001 <49328285+Ankitsharma0001@users.noreply.github.com>
2019-04-14 17:10:09 +0530

    Update Bankers.c

--- Bankers.c
+++ Bankers.c
@@ -1,45 +1,22 @@
 #include<stdio.h>
 int main()
 {
- int n;    //n number of process
+ int n;    // number of process
  int r;      // number of resources
- int i,j,k,cnt,cntt;
- int flag = 0;
- int avail[10],p[10];
- int need[10][10],alloc[10][10],max[10][10];
+ int i,j;
 
- printf("\nEnter number of process :");
- scanf("%d",&n);
- printf("\n Enter number of resources types available : ");
- scanf("%d",&r);
- printf("\nEnter number of resources :\n");
- for(i=0;i<r;i++)
- {  printf("R%d ",i+1);
-    scanf("%d",&avail[i]);
- }
-  
-  printf("\n Enter allocation matrix  \n");
-  for(i=0;i<n;i++)
-  {
-  printf("p%d",i+1);          p[i]=0;
-  for(j=0;j<r;j++)
-   {
-    scanf("%d",&alloc[i][j]);
-   }
-  }
-  
-  
-  printf("\n Enter MAX matrix  \n");
+ int need[10][10];
 
-  for(i=0;i<n;i++)
-  {
-   printf("p%d",i+1);
-   for(j=0;j<r;j++)
-   {
-    scanf("%d",&max[i][j]);
-   }
-  } 
+    n = 5;  
+    r = 4; 
+    int alloc[5][4] = { { 0, 0, 1, 2 },{ 1, 0, 0, 0 },{ 1,3,5,4 },{ 0,6,3,2 },{ 0, 0, 1,4 } };  
+    
+    int max[5][4] = { { 0,0,1,2 },{ 1,7,5,0 },{ 2,3,5,6 },{ 0,6,5,2 },{ 0,6,5,6 } };
+    
+    int avail[4] = { 1,5,2,0 };
 
+
+printf("\t\t Need Matrix\n");
   for(i=0;i<n;i++)
   {
    printf("\np%d\t",i+1) ;
@@ -49,23 +26,135 @@ int main()
     printf("\t%d",need[i][j]);
     }
    }
-///////////////////////////////////////////////////////////////   
   
-  for(i=0;i<1;i++)
-  {
-   for(j=0;j<r;j++)
-   {
-    if((need[i][j] < avail[j])){
-    	flag=1;
-	}
-	else
-	   flag=0;
-   } 
-   if(flag=1){
-   	
+  
+  int f1=0,f2=0,f3=0,f4=0,f5=0;
+  for(i=0;i<n;i++){
+  	 
+	 switch(i){
+     
+	   case 0:
+         if((need[i][0]<=avail[0]) && (need[i][1]<=avail[1]) && (need[i][2]<=avail[2]) && (need[i][3]<=avail[3])){
+	    	 f1=1;
+	    	 avail[0] = avail[0] + alloc[i][0];
+	    	 avail[1] = avail[1] + alloc[i][1];
+	    	 avail[2] = avail[2] + alloc[i][2];
+	    	 avail[3] = avail[3] + alloc[i][3];
+		 }
+	     else{
+	 	   
+	     }
+		 	     
+	     break;
+	     
+	    case 1:
+	    	if((need[i][0]<=avail[0]) && (need[i][1]<=avail[1]) && (need[i][2]<=avail[2])&& (need[i][3]<=avail[3])){
+	    	  f2=1;
+	    	  avail[0] = avail[0] + alloc[i][0];
+	    	  avail[1] = avail[1] + alloc[i][1];
+	    	  avail[2] = avail[2] + alloc[i][2];
+	    	  avail[3] = avail[3] + alloc[i][3];  
+	        }
+	        
+	        break;
+	        
+	    case 2:
+		   if((need[i][0]<=avail[0]) && (need[i][1]<=avail[1]) && (need[i][2]<=avail[2])&& (need[i][3]<=avail[3])){
+	    	  f3=1;
+	    	  avail[0] = avail[0] + alloc[i][0];
+	    	  avail[1] = avail[1] + alloc[i][1];
+	    	  avail[2] = avail[2] + alloc[i][2];
+	    	  avail[3] = avail[3] + alloc[i][3];  
+	        }
+	        
+	        break;
+			
+		case 3:
+		   if((need[i][0]<=avail[0]) && (need[i][1]<=avail[1]) && (need[i][2]<=avail[2])&& (need[i][3]<=avail[3])){
+	    	  f4=1;
+	    	  avail[0] = avail[0] + alloc[i][0];
+	    	  avail[1] = avail[1] + alloc[i][1];
+	    	  avail[2] = avail[2] + alloc[i][2];
+	    	  avail[3] = avail[3] + alloc[i][3];
+	         }
+	        
+	        break;
+			
+		case 4:
+		    if((need[i][0]<=avail[0]) && (need[i][1]<=avail[1]) && (need[i][2]<=avail[2])&& (need[i][3]<=avail[3])){
+	    	  f5=1;
+	    	  avail[0] = avail[0] + alloc[i][0];
+	    	  avail[1] = avail[1] + alloc[i][1];
+	    	  avail[2] = avail[2] + alloc[i][2];
+	    	  avail[3] = avail[3] + alloc[i][3]; 
+	        }
+	        
+	        break;		    
+	    	
+	    default :
+		     printf("\ndefault");	
+      }
+      
    }
-   
+  
+  if((f1 ==1) && (f2 ==1) && (f3 ==1) && (f4 ==1) && (f5 ==1) ){
+  	printf("\n\n All Executed");
   }
- 
- 
+  else{
+  	 if( f1 != 1){
+  	 	     f1=1;
+	         for(i=0;i<1;i++){
+                for(j=0;j<r;j++){
+                	avail[j] = avail[j] + alloc[i][j];
+               }
+             }
+	   }
+	  else if(f2 != 1){
+	  	    f2=1;
+	         for(i=1;i<2;i++){
+                for(j=0;j<r;j++){
+                	avail[j] = avail[j] + alloc[i][j];
+               }
+             }
+	    }
+	  else if(f3 != 1){
+	  	     f3=1;
+	         for(i=2;i<3;i++){
+                for(j=0;j<r;j++){
+                	avail[j] = avail[j] + alloc[i][j];
+               }
+             }
+	    }
+	  else if(f4 !=1){
+	  	      f4=1;
+	          for(i=3;i<4;i++){
+                for(j=0;j<r;j++){
+                	avail[j] = avail[j] + alloc[i][j];
+               }
+             }
+         }
+       else{
+       	     f5=1;
+	         for(i=4;i<5;i++){
+                for(j=0;j<r;j++){
+                	avail[j] = avail[j] + alloc[i][j];
+               }
+             }
+	     }      
+	  		   	 
+  }
+
+   printf("\n\n\n     Final   Avaliable :-- ");
+   
+              for(j=0;j<r;j++){
+			     printf("\t: %d ",avail[j]);
+               }
+            
+     if((f1 ==1) && (f2 ==1) && (f3 ==1) && (f4 ==1) && (f5 ==1) ){
+  	      printf("\n\n \t\t Safe State  ");
+      } 
+	  else
+	  {
+	  	printf("\n\n \t\t Not Safe State");
+			 }       
 }
